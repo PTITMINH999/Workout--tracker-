33f46223e5c83fa73ed9563511b26b2f88d37547
Minh999 <dangtuanminh064@gmail.com>
2025-10-27 22:10:47 +0700

    Change some lines in App.jsx

--- workout-tracker-frontend/src/App.jsx
+++ workout-tracker-frontend/src/App.jsx
@@ -1,5 +1,15 @@
-import PrivateRoute from "./components/PrivateRoute";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import ExercisesPage from "./pages/ExercisesPage";
+import WorkoutsPage from "./pages/WorkoutsPage";
+import UserManagement from "./pages/UserManagement";
+import ProgressPage from "./pages/ProgressPage";
+import StatsPage from "./pages/StatsPage";
 function App() {
   return (
     <>
@@ -8,61 +18,32 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
-        {/* Private pages */}
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/exercises"
-          element={
-            <PrivateRoute>
-              <ExercisesPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/workouts"
-          element={
-            <PrivateRoute>
-              <WorkoutsPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/users"
-          element={
-            <PrivateRoute>
-              <UserManagement />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/progress"
-          element={
-            <PrivateRoute>
-              <ProgressPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/stats"
-          element={
-            <PrivateRoute>
-              <StatsPage />
-            </PrivateRoute>
-          }
-        />
+        {/* Main pages */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/exercises" element={<ExercisesPage />} />
+        <Route path="/workouts" element={<WorkoutsPage />} />
 
-        {/* Default */}
-        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+        {/* ✅ User management page */}
+        <Route path="/users" element={<UserManagement />} />
+        <Route path="/progress" element={<ProgressPage />} />
+        <Route path="/stats" element={<StatsPage />} />
+        {/* Default redirect */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
 
-      <ToastContainer position="top-right" autoClose={2000} theme="colored" />
+      {/* Toast hiển thị toàn app */}
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="colored"
+      />
     </>
   );
 }
+
+export default App;
